766c6c10554a7b4de2a72cb7a5dc95b08bd0989e
Al Najar, Ismaeel <Ismaeel.AlNajar@vector.com>
2024-07-09 09:24:37 +0200

    SKA-547: Added timeouts and enhanced testing scripts (#51)

--- SocketCAN/demos/CANoe4SW_SE/run_all.sh
+++ SocketCAN/demos/CANoe4SW_SE/run_all.sh
@@ -3,6 +3,10 @@ scriptDir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 silKitDir=/home/vector/vfs/SILKit/SilKit-4.0.50-ubuntu-18.04-x86_64-gcc/
 
+# Ensure the logs directory exists
+logDir=$scriptDir/logs # define a directory for .out files
+mkdir -p $logDir # if it does not exist, create it
+
 # if "exported_full_path_to_silkit" environment variable is set (in pipeline script), use it. Otherwise, use default value
 silKitDir="${exported_full_path_to_silkit:-$silKitDir}"
 
@@ -21,21 +25,22 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Start the sil-kit-registry
-$silKitDir/SilKit/bin/sil-kit-registry --listen-uri 'silkit://0.0.0.0:8501' -s &> $scriptDir/sil-kit-registry.out &
-sleep 1 # wait 1 second for the creation/existense of the .out file
-timeout 30s grep -q 'Registered signal handler' <(tail -f /$scriptDir/sil-kit-registry.out) || { echo "[error] Timeout reached while waiting for sil-kit-registry to start"; exit 1; }
+$silKitDir/SilKit/bin/sil-kit-registry --listen-uri 'silkit://0.0.0.0:8501' -s &>  $logDir/sil-kit-registry.out &
+sleep 1 # wait 1 second for the creation of the .out file
+timeout 30s grep -q 'Registered signal handler' <(tail -f $logDir/sil-kit-registry.out -n +1) || { echo "[error] Timeout reached while waiting for sil-kit-registry to start"; exit 1; }
 
 # Start the SilKitDemoCanEchoDevice
-$scriptDir/../../../bin/sil-kit-demo-can-echo-device &> $scriptDir/sil-kit-demo-can-echo-device.out &
-timeout 30s grep -q 'Press CTRL + C to stop the process...' <(tail -f /$scriptDir/sil-kit-demo-can-echo-device.out) || { echo "[error] Timeout reached while waiting for sil-kit-demo-can-echo-device to start"; exit 1; }
+$scriptDir/../../../bin/sil-kit-demo-can-echo-device &> $logDir/sil-kit-demo-can-echo-device.out &
+sleep 1 # wait 1 second for the creation of the .out file
+timeout 30s grep -q 'Press CTRL + C to stop the process...' <(tail -f $logDir/sil-kit-demo-can-echo-device.out -n +1) || { echo "[error] Timeout reached while waiting for sil-kit-demo-can-echo-device to start"; exit 1; }
 
 # Run and test adapter with vCAN device of MTU 72
-$scriptDir/../shell_scripts/setup_vCAN_start_adapter_send_CAN_frames.sh &> $scriptDir/setup_vCAN_start_adapter_send_CAN_frames.out &
+$scriptDir/../shell_scripts/setup_vCAN_start_adapter_send_CAN_frames.sh &> $logDir/setup_vCAN_start_adapter_send_CAN_frames.out &
 script_pid=$!
-
+sleep 1 # wait 1 second for the creation of the .out file
+timeout 90s grep -q 'Adapter has been launched and CAN payload is being generated...' <(tail -f $logDir/setup_vCAN_start_adapter_send_CAN_frames.out -n +1) || { echo "[error] Timeout reached while waiting for (setup_vCAN_start_adapter_send_CAN_frames.sh) to start"; exit 1; }
 
 $scriptDir/run.sh
-#capture returned value of run_mtu72.sh script
 exit_status=$?
 
 if [[ exit_status -ne 0 ]]; then
@@ -45,9 +50,11 @@ fi
 kill $script_pid
 
 # Run and test adapter with vCAN device of MTU 16
-$scriptDir/../shell_scripts/setup_vCAN_start_adapter_send_CAN_frames.sh -mtu16 &> $scriptDir/setup_vCAN_start_adapter_send_CAN_frames-mtu16.out &
+$scriptDir/../shell_scripts/setup_vCAN_start_adapter_send_CAN_frames.sh -mtu16 &> $logDir/setup_vCAN_start_adapter_send_CAN_frames-mtu16.out &
+sleep 1 # wait 1 second for the creation of the .out file
+timeout 60s grep -q 'Adapter has been launched and CAN payload is being generated...' <(tail -f $logDir/setup_vCAN_start_adapter_send_CAN_frames-mtu16.out -n +1) || { echo "[error] Timeout reached while waiting for (setup_vCAN_start_adapter_send_CAN_frames.sh -mtu16) to start"; exit 1; }
+
 $scriptDir/run.sh -mtu16
-#capture returned value of run_mtu16.sh script
 exit_status=$?
 
 if [[ exit_status -ne 0 ]]; then
@@ -55,15 +62,15 @@ if [[ exit_status -ne 0 ]]; then
 fi
 
 echo "sil-kit-registry.out:--------------------------------------------------------------------------------------" 
-cat $scriptDir/sil-kit-registry.out
+cat $logDir/sil-kit-registry.out
 echo "-----------------------------------------------------------------------------------------------------------" 
 
 echo "setup_vCAN_start_adapter_send_CAN_frames.out:--------------------------------------------------------------" 
-cat $scriptDir/setup_vCAN_start_adapter_send_CAN_frames.out
+cat $logDir/setup_vCAN_start_adapter_send_CAN_frames.out
 echo "-----------------------------------------------------------------------------------------------------------" 
 
 echo "setup_vCAN_start_adapter_send_CAN_frames-mtu16.out:--------------------------------------------------------"
-cat $scriptDir/setup_vCAN_start_adapter_send_CAN_frames-mtu16.out
+cat $logDir/setup_vCAN_start_adapter_send_CAN_frames-mtu16.out
 echo "-----------------------------------------------------------------------------------------------------------"
 
 #exit run_all.sh with same exit_status
--- SocketCAN/demos/shell_scripts/setup_vCAN_start_adapter_send_CAN_frames.sh
+++ SocketCAN/demos/shell_scripts/setup_vCAN_start_adapter_send_CAN_frames.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+scriptDir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+logDir=$scriptDir/../CANoe4SW_SE/logs # define a directory for .out files
+mkdir -p $logDir # if it does not exist, create it
 
 # cleanup trap for child processes 
 trap 'kill $(jobs -p); exit' EXIT SIGHUP;
@@ -19,24 +22,28 @@ else
 fi
 
 # Setup an FD-capable vCAN device (default case)
-echo "Setting up [can0]..."
-$SCRIPT_DIR/setup_vCAN_device.sh can0 $arg &> $SCRIPT_DIR/setup_vCAN_device_can0$arg.out
+echo "[info] Setting up [can0]..."
+$scriptDir/setup_vCAN_device.sh can0 $arg &> $logDir/setup_vCAN_device_can0$arg.out
 
 # Start adapter 
-echo "Starting sil-kit-adapter-vcan..."
-$SCRIPT_DIR/../../../bin/sil-kit-adapter-vcan &> $SCRIPT_DIR/sil-kit-adapter-vcan.out &
-adapter_pid=$!
-
+echo "[info] Starting sil-kit-adapter-vcan..."
+$scriptDir/../../../bin/sil-kit-adapter-vcan --name SilKitAdapterSocketCAN$arg &> $logDir/sil-kit-adapter-vcan$arg.out &
 sleep 1 # wait 1 second for the creation/existence of the .out file
+timeout 30s grep -q 'Press CTRL + C to stop the process...' <(tail -f $logDir/sil-kit-adapter-vcan$arg.out -n +1) || { echo "[error] Timeout reached while waiting for sil-kit-adapter-vcan to start"; exit 1; }
+echo "[info] sil-kit-adapter-vcan has been started"
 
-timeout 30s grep -q 'Press CTRL + C to stop the process...' <(tail -f /$SCRIPT_DIR/sil-kit-adapter-vcan.out) || { echo "[error] Timeout reached while waiting for sil-kit-adapter-vcan to start"; exit 1; }
-echo "sil-kit-adapter-vcan has been started"
+echo "[info] Sending Classical CAN frames on [can0]..."
+$scriptDir/send_CAN_frames.sh can0 &> $logDir/send_ClassicalCAN_frames.out &
+sleep 1
+timeout 30s grep -q '\{001#AA.AA.BB.BB\} sent to \[can0\]' <(tail -f $logDir/send_ClassicalCAN_frames.out -n +1) || { echo "[error] Timeout reached while waiting for (send_CAN_frames.sh can0) to start"; exit 1; }
 
+# Only send CANFD frames if vCAN device is compatible (not -mtu arg is passed to script)
+if [[ -z $arg ]]; then
+  echo "[info] Sending CAN FD frames on [can0]..."
+  $scriptDir/send_CAN_frames.sh can0 -fd &> $logDir/send_CANFD_frames.out &
+  sleep 1
+  timeout 30s grep -q '\{005##0.AA.AA.AA.AA.BB.BB.BB.BB.CC.CC.CC.DD\} sent to \[can0\]' <(tail -f $logDir/send_CANFD_frames.out -n +1) || { echo "[error] Timeout reached while waiting for (send_CAN_frames.sh can0 -fd) to start"; exit 1; }
+fi
 
-# Send frames on [can0] 
-echo "Sending Classical CAN frames on [can0]..."
-$SCRIPT_DIR/send_CAN_frames.sh can0 &> $SCRIPT_DIR/send_ClassicalCAN_frames.out &
-echo "Sending CAN FD frames on [can0]..."
-$SCRIPT_DIR/send_CAN_frames.sh can0 -fd &> $SCRIPT_DIR/send_CANFD_frames.out &
-
+echo "[info] Adapter has been launched and CAN payload is being generated..."
 sleep 15
